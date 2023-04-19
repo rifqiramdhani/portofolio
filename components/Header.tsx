cd3a2ca820eac7c5f149247f140b1a54d422774a
Rifqi Ramdhani <rifqiramdhani71@yahoo.co.id>
2023-04-20 06:48:42 +0700

    responsive header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import { BsFacebook, BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs"
 export default function Header() {
     return (
         <section className="bg-[#023047] min-h-screen">
-            <div className="container mx-auto pt-8 px-5 lg:px-10 md:px-10">
+            <div className="container mx-auto pt-8 px-5 lg:px-10 md:px-10 xl:px-0">
                 {/* Navbar */}
                 <Navbar />
                 {/* </Navbar */}
@@ -16,18 +16,31 @@ export default function Header() {
                 <div className="flex flex-wrap justify-center items-center mt-5 md:mt-16 xl:mt-20">
 
                     <div className="w-full md:hidden">
-                        <Image src="/img/photo-header.svg" width={100} height={100} alt="My picture" className="w-full h-full md:w-3/5 mx-auto" priority={true} />
+                        <Image src="./img/photo-header.svg" width={100} height={100} alt="My picture" className="w-full h-full md:w-3/5 mx-auto" priority={true} />
 
                         <div className="absolute right-0 top-20">
                             <div className="flex items-center">
                                 <div className="w-1/5">
-                                    <BsFacebook className="w-[15px] h-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
-                                    <BsGithub className="w-[15px] h-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
-                                    <BsInstagram className="w-[15px] h-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
-                                    <BsLinkedin className="w-[15px] h-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
+
+                                    <a href="https://www.facebook.com/rifqi.li.21/?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer">
+                                        <BsFacebook className="w-[12px] h-[12px] mobile-m:h-[15px] mobile-m:w-[15px] mobile-l:h-[15px] mobile-l:w-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
+                                    </a>
+
+                                    <a href="https://github.com/rifqiramdhani" target="_blank" rel="noopener noreferrer">
+                                        <BsGithub className="w-[12px] h-[12px] mobile-m:h-[15px] mobile-m:w-[15px] mobile-l:h-[15px] mobile-l:w-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
+                                    </a>
+
+                                    <a href="https://www.instagram.com/rifqi_li/" target="_blank" rel="noopener noreferrer">
+                                        <BsInstagram className="w-[12px] h-[12px] mobile-m:h-[15px] mobile-m:w-[15px] mobile-l:h-[15px] mobile-l:w-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
+                                    </a>
+
+                                    <a href="https://www.linkedin.com/in/rifqi-ramdhani-5b49b1208/" target="_blank" rel="noopener noreferrer">
+                                        <BsLinkedin className="w-[12px] h-[12px] mobile-m:h-[15px] mobile-m:w-[15px] mobile-l:h-[15px] mobile-l:w-[15px] my-3 sm:h-[22px] sm:w-[22px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
+
+                                    </a>
                                 </div>
                                 <div className="w-full">
-                                    <p className="text-xs font-medium rotate-90 text-center -ml-4 sm:text-sm">Folow <span className="font-bold">Me</span> On</p>
+                                    <p className="text-[0.6rem] font-medium rotate-90 text-center -ml-4 sm:text-sm">Folow <span className="font-bold">Me</span> On</p>
                                 </div>
                             </div>
                         </div>
@@ -35,7 +48,7 @@ export default function Header() {
 
 
                     <div className="w-full md:w-6/12">
-                        <Image src="/img/polygon.svg" alt="Image polygon" width={136} height={136} className="w-20 h-20 absolute z-0 top-[63%] mobile-m:top-[69%] mobile-l:top-[76%] lg:w-[136px] lg:h-[136px] lg:left-6 xl:left-10 md:w-[93px] md:h-[93px] md:top-[161px]"/>
+                        <Image src="./img/polygon.svg" alt="Image polygon" width={136} height={136} className="w-20 h-20 absolute z-0 top-[61%] mobile-m:top-[69%] mobile-l:top-[76%] lg:w-[136px] lg:h-[136px] lg:left-6 xl:left-10 md:w-[93px] md:h-[93px] md:top-[161px] xl:top-[46%]"/>
                         
                         <div className="relative z-1 top-10">
                             <h1 className="text-xl font-medium xl:text-4xl my-2 sm:text-2xl md:text-2xl lg:text-2xl">Hi, I&apos;m Moch Rifqi Ramdhani</h1>
@@ -52,17 +65,29 @@ export default function Header() {
                     <div className="w-6/12 hidden md:block">
                         <div className="flex relavite">
 
-                            <div className="w-full md:w-11/12 lg:w-11/12">
-                                <Image src="/img/photo-header.svg" width={100} height={100} alt="My picture" className="w-full h-full" />
+                            <div className="w-full md:w-11/12 lg:w-11/12 xl:w-full">
+                                <Image src="./img/photo-header.svg" width={100} height={100} alt="My picture" className="w-full h-full" />
                             </div>
 
-                            <div className="absolute right-0 md:top-20">
+                            <div className="absolute right-0 md:top-24 xl:right-[46px] xl:top-32">
                                 <div className="flex items-center">
                                     <div className="w-1/5">
-                                        <BsFacebook className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
-                                        <BsGithub className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
-                                        <BsInstagram className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
-                                        <BsLinkedin className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px]" />
+                                        <a href="https://www.facebook.com/rifqi.li.21/?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer">
+                                            <BsFacebook className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px] xl:w-30px] xl:h-[30px]" />
+                                        </a>
+
+                                        <a href="https://github.com/rifqiramdhani" target="_blank" rel="noopener noreferrer">
+                                            <BsGithub className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px] xl:w-30px] xl:h-[30px]" />
+                                        </a>
+
+                                        <a href="https://www.instagram.com/rifqi_li/" target="_blank" rel="noopener noreferrer">
+                                            <BsInstagram className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px] xl:w-30px] xl:h-[30px]" />
+                                        </a>
+
+                                        <a href="https://www.linkedin.com/in/rifqi-ramdhani-5b49b1208/" target="_blank" rel="noopener noreferrer">
+                                            <BsLinkedin className="w-[15px] h-[15px] my-10 sm:h-[22px] sm:w-[22px] md:h-[19px] md:w-[19px] sm:my-5 lg:w-[22px] lg:h-[22px] xl:w-30px] xl:h-[30px]" />
+
+                                        </a>
                                     </div>
                                     <div className="w-full">
                                         <p className="text-sm font-medium rotate-90 text-center -ml-4">Folow <span className="font-bold">Me</span> On</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,15 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    screens: {
+      'mobile-m' : '375px',
+      'mobile-l' : '425px',
+      'sm' : '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
